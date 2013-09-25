7e89c35bb126ce2b39a3f0f421ee74232d034658
Cori Allen <coriallen@gmail.com>
2013-09-25 16:39:23 -0400

    Work done in class for example.

--- advanced_hash.rb
+++ advanced_hash.rb
@@ -1,3 +1,4 @@
+require 'pry'
 #Lets check out some more advanced uses of hashes
 
 # we can actually have a hash as a hash value!
@@ -21,7 +22,7 @@ person = {
 
 # we can even have arrays as hash values!
 
-student = {
+Amir = {
 	name: {
 		first: 'Amir',
 		last: 'Jacobs'
@@ -34,6 +35,57 @@ student = {
 	}
 }
 
+Sally = {
+	name: {
+		first: 'Sally',
+		last: 'Smith'
+	},
+	grades: [92, 99, 91, 96, 100, 91],
+	age: 19,
+	family: {
+		size: 3,
+		members: ['Joe', 'Aimee', 'Barack']
+	}
+}
+
+students = [Amir, Sally]
+# grades = student[:grades]
+def get_grades(students)
+	all_grades = []
+	
+	students.each do |student|
+		all_grades << student[:grades]
+	end
+	return all_grades.flatten.sort
+end
+
+def avg(numbers)
+	sum = 0
+	numbers.each do |number|
+		sum += number.to_f
+	end
+	sum/numbers.length
+end
+
+total_grades = get_grades(students)
+average = avg(total_grades)
+
+binding.pry
+
 #how would we get the student's third grade?
+#to put all the grades together into a single array rather than two arrays 
+# inside one array called all_grades, use array.flatten 
 
 #how would we get the second member of this person's family?
+
+
+
+
+
+
+
+
+
+
+
+
